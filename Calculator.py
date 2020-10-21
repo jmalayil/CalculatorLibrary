f31fed06ec54932eaa1b2be7409cd5ba5a9969de
Saju Malayil <Saju.Malayil@gmail.com>
2020-10-21 07:02:11 -0400

    Add unit test for calculator

--- Calculator.py
+++ Calculator.py
@@ -3,8 +3,14 @@ calculator.py
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
